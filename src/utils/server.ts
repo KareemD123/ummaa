405ca112e5ea92d31887d07eb49c0ddb8bcf0fb3
KareemD123 <kareemdraz.careers@gmail.com>
2025-08-24 00:35:53 -0400

    initial commit

---
+++ src/utils/server.ts
@@ -0,0 +1,7 @@
+const dev = process.env.NODE_ENV !== "production";
+
+export const server = "http://localhost:3000";
+
+// export const server = dev
+//   ? "http://localhost:3000"
+//   : "https://next-ecommerce-front.vercel.app";
